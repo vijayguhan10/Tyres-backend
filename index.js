116210b8a0ffee8cb8bb69e90137cab0c1bfceab
vijayguhan10 <vijayguhan10@gmail.com>
2025-05-13 01:24:59 +0530

    updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://revozen-partner.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     exposedHeaders: ["set-cookie"],
