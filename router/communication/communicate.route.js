9e765f4f52a383f2a0885deb0a83f966b6683149
vijayguhan10 <vijayguhan10@gmail.com>
2025-07-04 14:28:34 +0530

    comitted with the changes of the optin

--- router/communication/communicate.route.js
+++ router/communication/communicate.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const CommunicateController = require('../../Controllers/communication/Communicate.controller');
+router.get('/communication/optin', (req, res) => {
+    res.status(200).send('Webhook is live ✅');
+});
 
 router.post('/optin', CommunicateController.saveOptIn);
+
 router.post('/message', CommunicateController.receiveMessage);
 
 module.exports = router;
